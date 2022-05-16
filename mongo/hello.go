cc2f19993b654c7162a4dd02ac959c1f3fe5218a
clz-lab <fadinglight9291117@126.com>
2022-05-16 18:49:22 +0800

    mongo but not finish

--- mongo/hello.go
+++ mongo/hello.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 func main() {
-	fmt.Println("Hello World!");
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Println("You must set your 'MONGODB_URI' envionmental variable.")
+	}
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	col := client.Database("sample_mflix").Collection("movies")
+	title := "Back to the Future"
+
+	var result bson.M
+	err = col.FindOne(context.TODO(), bson.D{{"title", title}}).Decode(&result)
+
 }
